3c90550e64758c2f6b39106c58c72df116afcf33
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-29 18:22:36 -0500

    Update: Add proper annotation in MedicoController class and implemented a new method

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,7 @@
 package med.voll.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
-    
+    @PostMapping
+    public void registrarMedico(@RequestBody String parametro) {
+     System.out.println("El request llega correctamente");
+     System.out.println(parametro);   
+    }
     
 }
