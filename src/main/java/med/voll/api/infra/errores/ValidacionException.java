cb09c2e50e4a233d17e06df43d70951ab32c938d
Victor Perdomo <victorgabperdomo@gmail.com>
2025-01-11 22:15:35 -0500

    Feat: Create class `TratadorDeErrores` to handle errors in the app such as Error404 and Error400
    - Create `ValidacionException`

---
+++ src/main/java/med/voll/api/infra/errores/ValidacionException.java
@@ -0,0 +1,8 @@
+package med.voll.api.infra.errores;
+
+public class ValidacionException extends RuntimeException {
+
+    public ValidacionException(String mensaje) {
+        super(mensaje);
+    }
+}
