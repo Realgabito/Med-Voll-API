b7d252e6466af320b23c926f864f58666f249b10
Victor Perdomo <victorgabperdomo@gmail.com>
2025-01-11 21:54:11 -0500

    Update: Add information in `DatosRegistroPaciente `
    Update `MedicoRepository` adding a Query to seach in database

--- src/main/java/med/voll/api/DTO/paciente/Paciente.java
+++ src/main/java/med/voll/api/DTO/paciente/Paciente.java
@@ -1,4 +1,60 @@
-package med.voll.api.DTO;
+package med.voll.api.DTO.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.DTO.Direccion;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
 
 public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String documentoIdentidad;
+    private String telefono;
+
+    @Embedded
+    private Direccion direccion;
+
+    private Boolean activo;
+
+    public Paciente(DatosRegistroPaciente datos) {
+        this.activo = true;
+        this.nombre = datos.nombre();
+        this.email = datos.email();
+        this.telefono = datos.telefono();
+        this.documentoIdentidad = datos.documentoIdentidad();
+        this.direccion = new Direccion(datos.direccion());
+
+    }
+
+    public void actualizarInformacion(DatosActualizacionPaciente datosActualizacionPaciente) {
+        if (datosActualizacionPaciente.nombre() != null) {
+            this.nombre = datosActualizacionPaciente.nombre();
+        }
+
+        if (datosActualizacionPaciente.telefono() != null) {
+            this.telefono = datosActualizacionPaciente.telefono();
+        }
+
+        if (datosActualizacionPaciente.direccion() != null) {
+            this.direccion.actualizarInformacion(datosActualizacionPaciente.direccion());
+        }
+    }
+
+    public void eliminarPaciente() {
+        this.activo = false;
+    }
 }
--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -1,14 +1,37 @@
 package med.voll.api.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
-import med.voll.api.DTO.Medico;
+import med.voll.api.DTO.medico.Especialidad;
+import med.voll.api.DTO.medico.Medico;
 
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findByActivoTrue(Pageable paginacion);
+
+    @Query("""
+            SELECT m FROM Medico m
+            WHERE m.activo = true
+            AND m.especialidad = :especialidad
+            AND m.id not in(SELECT c.medico.id FROM Consulta c WHERE c.fecha = :fecha)
+            ORDER BY  rand()
+            LIMIT 1
+            """)
+    Medico elegirMedicoAleatorio(@Param("especialidad")Especialidad especialidad,@Param("fecha") LocalDateTime fecha);
+
+
+    @Query("""
+            SELECT m.activo FROM Medico m WHERE m.id = :idMedico
+            """)
+    boolean findActivoById(Long idMedico);
     
 }
